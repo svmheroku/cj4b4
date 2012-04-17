273aa0240c9fc1b453543419ced14bbace287dc2
svm heroku <svmheroku@yahoo.com>
2012-04-17 08:52:58 +0100

    predictions/us_stk_past/us_stk_past_png_yr.bash

--- predictions/us_stk_past/us_stk_past_png_yr.bash
+++ predictions/us_stk_past/us_stk_past_png_yr.bash
@@ -7,11 +7,6 @@
 # I use this script to create a png file for the splash page.
 # The png file shows a weekly summary of DanBot performance.
 
-# Using 2012 as a pattern,
-# assume that us_stk_past_yr.sql 2012 has been run and CSV data landed here:
-# /tmp/us_stk_sunday_l_2012.lst
-# /tmp/us_stk_sunday_s_2012.lst
-
 . /pt/w/b4/cj4b4/.cj
 # Start by running us_stk_past_yr.sql to fill 2 txt files:
 # /tmp/us_stk_sunday_l_${1}.lst
@@ -26,12 +21,13 @@ sqt>/tmp/us_stk_past_yr_sql_out.txt<<EOF
 EOF
 
 echo 'WK, WEEK_OF, RROWNUM, PREDICTION_COUNT, SUM_G24HR, CUM_SUM' > /tmp/us_stk_sunday_l_${1}.csv
-echo 'WK, WEEK_OF, RROWNUM, PREDICTION_COUNT, SUM_G24HR, CUM_SUM' > /tmp/us_stk_sunday_l_${1}.csv
+echo 'WK, WEEK_OF, RROWNUM, PREDICTION_COUNT, SUM_G24HR, CUM_SUM' > /tmp/us_stk_sunday_s_${1}.csv
 
 grep '^201' /tmp/us_stk_sunday_l_${1}.lst >> /tmp/us_stk_sunday_l_${1}.csv
-grep '^201' /tmp/us_stk_sunday_s_${1}.lst >> /tmp/us_stk_sunday_l_${1}.csv
+grep '^201' /tmp/us_stk_sunday_s_${1}.lst >> /tmp/us_stk_sunday_s_${1}.csv
 
 /usr/bin/R -f us_stk_past_png_yr.r $1
+
 cp /tmp/us_stk_sunday${1}.png /pt/w/b4/b4/public/images/
 
 exit 0
---
+++ predictions/us_stk_past/us_stk_past_yr.sql
@@ -0,0 +1,267 @@
+--
+-- us_stk_past_yr.sql
+--
+
+-- Demo:
+-- sqt @us_stk_past_yr.sql 2012
+
+-- I use this script to join gains with scores for a specific year passed on the command line.
+
+-- This script sometimes depends on ibs5min_cpy.bash
+-- which fills ibs5min_cpy with recent data from ibs5min from both z2, z3.
+
+-- Using 2012 as an example, the output I'm interested in lands here:
+-- /tmp/us_stk_sunday_l_2012.lst
+-- /tmp/us_stk_sunday_s_2012.lst
+
+-- I need dates to match-up to the second:
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD hh24:mi:ss';
+
+-- Start by getting a copy of stkscores:
+
+PURGE RECYCLEBIN;
+DROP TABLE ibs5min_sun1;
+
+DROP TABLE ibs5min_sun;
+
+DROP TABLE stkscores_sunl;
+
+DROP TABLE stkscores_suns;
+
+DROP TABLE stkscores_sunlj;
+
+DROP TABLE stkscores_sunsj;
+
+DROP TABLE us_stk_sunday_l;
+
+DROP TABLE us_stk_sunday_s;
+
+-- ibs5min_cpy might have dups so I filter them out.
+CREATE TABLE ibs5min_sun1 COMPRESS AS
+SELECT
+tkr,ydate
+,AVG(clse)clse
+FROM ibs5min_cpy
+GROUP BY tkr,ydate
+ORDER BY tkr,ydate
+/
+
+-- The de-duped data is handy so I copy it into ibs5min_cpy:
+TRUNCATE TABLE ibs5min_cpy;
+INSERT INTO ibs5min_cpy(tkr,ydate,clse)SELECT tkr,ydate,clse FROM ibs5min_sun1;
+
+-- ok now get to work:
+CREATE TABLE ibs5min_sun COMPRESS AS
+SELECT
+tkr
+,ydate
+,clse                                                           price_0hr
+, LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)     price_1hr
+,(LEAD(clse,12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)-clse) g1hr
+,LEAD(clse, 6.5*12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate) price_24hr
+,LEAD(ydate,6.5*12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate) selldate
+,(LEAD(clse,6.5*12*1,NULL)OVER(PARTITION BY tkr ORDER BY ydate)-clse) g24hr
+FROM ibs5min_sun1
+WHERE TO_CHAR(ydate,'YYYY') = '&1'
+ORDER BY tkr,ydate
+/
+
+CREATE TABLE stkscores_sunl COMPRESS AS
+SELECT
+tkr
+,ydate
+,AVG(score)score
+FROM stkscores
+WHERE targ = 'gatt'
+AND TO_CHAR(ydate,'YYYY') = '&1'
+GROUP BY tkr,ydate
+ORDER BY tkr,ydate
+/
+
+CREATE TABLE stkscores_suns COMPRESS AS
+SELECT
+tkr
+,ydate
+,AVG(score)score
+FROM stkscores
+WHERE targ = 'gattn'
+AND TO_CHAR(ydate,'YYYY') = '&1'
+GROUP BY tkr,ydate
+ORDER BY tkr,ydate
+/
+
+-- Now join em to get bullish scores:
+
+CREATE TABLE stkscores_sunlj COMPRESS AS
+SELECT
+l.tkr
+,l.ydate
+,l.score - s.score score
+FROM stkscores_sunl l, stkscores_suns s
+WHERE l.tkr||l.ydate = s.tkr||s.ydate
+AND l.score - s.score > 0.55
+AND l.score > 0.55
+AND s.score < 0.45
+ORDER BY s.tkr,s.ydate
+/
+
+-- Now join em to get bearish scores:
+
+CREATE TABLE stkscores_sunsj COMPRESS AS
+SELECT
+l.tkr
+,l.ydate
+,l.score - s.score score
+FROM stkscores_sunl l, stkscores_suns s
+WHERE l.tkr||l.ydate = s.tkr||s.ydate
+AND l.score - s.score < -0.55
+AND s.score > 0.55
+AND l.score < 0.45
+ORDER BY s.tkr,s.ydate
+/
+
+-- Indexes help speed up future joins:
+CREATE INDEX ibs5min_suni1 ON ibs5min_sun(tkr,ydate);
+CREATE INDEX stkscores_sunsji1 ON stkscores_sunsj(tkr,ydate);
+CREATE INDEX stkscores_sunlji1 ON stkscores_sunlj(tkr,ydate);
+
+-- Statistics help speed up future joins:
+ANALYZE TABLE ibs5min_sun     ESTIMATE STATISTICS SAMPLE 11 PERCENT;
+ANALYZE TABLE stkscores_sunsj ESTIMATE STATISTICS SAMPLE 11 PERCENT;
+ANALYZE TABLE stkscores_sunlj ESTIMATE STATISTICS SAMPLE 11 PERCENT;
+
+-- rpt
+SELECT MIN(ydate),                      COUNT(ydate),MAX(ydate)FROM ibs5min_sun;
+SELECT MIN(ydate),MIN(score),MAX(score),COUNT(ydate),MAX(ydate)FROM stkscores_sunlj;
+SELECT MIN(ydate),MIN(score),MAX(score),COUNT(ydate),MAX(ydate)FROM stkscores_sunsj;
+
+-- Now join bullish scores with gains:
+
+CREATE TABLE us_stk_sunday_l COMPRESS AS
+SELECT
+i.tkr
+,i.ydate
+,price_0hr
+,price_1hr
+,g1hr
+,price_24hr
+,selldate
+,g24hr
+,score
+,COVAR_POP(score,g24hr)OVER(PARTITION BY s.tkr ORDER BY s.ydate ROWS BETWEEN 12*24*5 PRECEDING AND CURRENT ROW)rnng_crr1
+FROM ibs5min_sun i, stkscores_sunlj s
+WHERE i.tkr = s.tkr AND i.ydate = s.ydate
+ORDER BY i.tkr,i.ydate
+/
+
+-- Now join bearish scores with gains:
+
+CREATE TABLE us_stk_sunday_s COMPRESS AS
+SELECT
+i.tkr
+,i.ydate
+,price_0hr
+,price_1hr
+,g1hr
+,price_24hr
+,selldate
+,g24hr
+,score
+,COVAR_POP(score,g24hr)OVER(PARTITION BY s.tkr ORDER BY s.ydate ROWS BETWEEN 12*24*5 PRECEDING AND CURRENT ROW)rnng_crr1
+FROM ibs5min_sun i, stkscores_sunsj s
+WHERE i.tkr = s.tkr AND i.ydate = s.ydate
+ORDER BY i.tkr,i.ydate
+/
+
+-- Get bullish CSV data:
+SPOOL /tmp/us_stk_sunday_l_&1
+
+COLUMN sum_g24hr FORMAT 9999999.99
+COLUMN cum_sum   FORMAT 9999999.99
+SET COLSEP ","
+
+SELECT
+wk
+,TO_CHAR(week_of,'YYYY-MM-DD')  week_of
+,rownum                         rrownum
+,prediction_count
+,sum_g24hr
+,SUM(sum_g24hr)OVER(ORDER BY wk)cum_sum
+FROM
+(
+  SELECT
+  TO_CHAR(ydate,'YYYY-WW') wk
+  ,MIN(ydate)              week_of
+  ,COUNT(g24hr)            prediction_count
+  ,SUM(g24hr)              sum_g24hr
+  FROM us_stk_sunday_l
+  WHERE rnng_crr1 > 0
+  GROUP BY TO_CHAR(ydate,'YYYY-WW')
+  ORDER BY TO_CHAR(ydate,'YYYY-WW')
+)
+/
+
+SPOOL OFF
+
+-- Get bearish CSV data:
+SPOOL /tmp/us_stk_sunday_s_&1
+
+SELECT
+wk
+,TO_CHAR(week_of,'YYYY-MM-DD')  week_of
+,rownum                         rrownum
+,prediction_count
+,sum_g24hr
+,SUM(sum_g24hr)OVER(ORDER BY wk)cum_sum
+FROM
+(
+  SELECT
+  TO_CHAR(ydate,'YYYY-WW') wk
+  ,MIN(ydate)              week_of
+  ,COUNT(g24hr)            prediction_count
+  ,SUM(g24hr)              sum_g24hr
+  FROM us_stk_sunday_s
+  WHERE rnng_crr1 > 0
+  GROUP BY TO_CHAR(ydate,'YYYY-WW')
+  ORDER BY TO_CHAR(ydate,'YYYY-WW')
+)
+/
+
+SPOOL OFF
+
+
+-- This SELECT gives me text for a-tags
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+SET TIME off TIMING off ECHO off HEADING off
+SET MARKUP HTML ON TABLE "id='table_us_stk_past'" ENTMAP ON
+SPOOL /tmp/_us_stk_past_spool.html.erb
+
+SELECT
+'Week: '||MIN(ydate)||' Through '||MAX(ydate) wweek
+FROM us_stk_sunday_s
+WHERE price_24hr > 0
+AND rnng_crr1 > 0
+GROUP BY TO_CHAR(ydate,'YYYY'),TO_CHAR(ydate,'WW')
+ORDER BY MIN(ydate)
+/
+
+SPOOL OFF
+SET MARKUP HTML OFF
+
+-- This SELECT gives me syntax to run a series of SQL scripts.
+-- Each script will give me data for 1 week.
+
+SPOOL /tmp/us_stk_past_week.txt
+SELECT
+'@us_stk_past_week.sql '||MIN(ydate) cmd
+FROM us_stk_sunday_s
+WHERE price_24hr > 0
+AND rnng_crr1 > 0
+-- I have 2011 already
+AND ydate>'2012-01-07'
+GROUP BY TO_CHAR(ydate,'YYYY'),TO_CHAR(ydate,'WW')
+ORDER BY MIN(ydate)
+/
+SPOOL OFF
+
+exit
